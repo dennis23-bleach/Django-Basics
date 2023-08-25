566ce2f617dac0d83e276020f1ee098c35c5802b
Dennis Samson <dennis.23bleach@gmail.com>
2023-08-26 01:21:00 +0530

    First Django push

---
+++ django_project/users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistraionForm
+
+# Create your views here.
+def register(request):
+    if request.method =='POST':
+        form = UserRegistraionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Your Account has been created. You are now able to login!")
+            return redirect('login')
+    else:
+        form = UserRegistraionForm()
+    return render(request, 'users/register.html', {'form' : form})
